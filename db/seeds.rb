7e1be9f29a9fb0ff26f1033aacff5f921c684997
VuDej <drvu47@gmail.com>
2023-02-07 13:17:00 +0100

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+require 'ffaker'
+
+AdminUser.create!(phone: '11111111111', password: 'admin1', password_confirmation: 'admin1')
+doctor = Doctor.create!(phone: '22222222222', password: 'doctor', password_confirmation: 'doctor',
+                        full_name: FFaker::Name.name, category: FactoryBot.create(:category))
+user = User.create!(phone: '33333333333', password: 'user11', password_confirmation: 'user11',
+                    full_name: FFaker::Name.name)
+appointments = FactoryBot.create_list(:appointment, 2, doctor: doctor, user: user)
+FactoryBot.create(:recommendation, appointment: appointments.sample)
