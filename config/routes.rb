ede49597886cf2efe9573abad1482ccfefd9f8b8
VuDej <drvu47@gmail.com>
2023-02-07 11:51:14 +0100

    controlelrs and routes, module devisable

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :doctors
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :doctors
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#welcome'
+
+  namespace :users do
+    resources :doctors, only: %i[index]
+    resources :appointments, only: %i[index show new create]
+  end
+
+  namespace :doctors do
+    resources :users, only: %i[index]
+    resources :appointments, only: %i[index show] do
+      resources :recommendations, only: %i[new create]
+    end
+  end
 end
